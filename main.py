bea555431d135766ad334816eccce025d013c009
LeonKrasniqii <redonpc14@gmail.com>
2025-06-24 20:34:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ def main():
 
 if __name__=="__main__":
     main()
+
+
+if st.button("Click Me"):
+    st.write("Button Clicked")
+
+if st.checkbox("Check Me"):
+    st.write("The check box has been checked")
+
+user_input = st.text_input("Enter Text","Sample Text")
+
+st.write("You enterd", user_input)
+
+age = st.number_input("Enter your age", min_value=0 , max_value = 100)
+st.write(f"Your age is:{age}")
+
+message = st.text_area("Enter a message")
+st.write(f"Your message is:{message}")
